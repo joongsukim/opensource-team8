2529bdefb01a8c5cc88e09a188e92dec62adc9d0
KIM SOYOUNG <ksyddr@gmail.com>
2021-12-02 05:26:56 +0900

    마무리

--- imgupload/urls.py
+++ imgupload/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
+from django.urls import path
 from . import views
 
 urlpatterns = [
